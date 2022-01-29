bf0e23c34bc66749f999725168883cfff1f8f4e9
Antoine Roumilhac <antoine.roumilhac@outlook.fr>
2022-01-29 19:36:27 +0100

    Renamed to bconv

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{env, process};
 
-use base_converter::Config;
+use bconv::Config;
 
 fn main() {
     let config = Config::new(env::args()).unwrap_or_else(|e| {
@@ -8,7 +8,7 @@ fn main() {
         process::exit(1);
     });
     
-    if let Err(e) = base_converter::run(config) {
+    if let Err(e) = bconv::run(config) {
         eprintln!("Application error: {}", e);
         process::exit(1);
     }
